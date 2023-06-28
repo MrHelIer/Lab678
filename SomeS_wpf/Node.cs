f109481b461c99f9557b268cf566b596c761eeb4
MrHeller <39415865+MrHelIer@users.noreply.github.com>
2023-06-28 11:08:26 +0200

    Create Node.cs

---
+++ SomeS_wpf/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace SomeS_wpf
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
